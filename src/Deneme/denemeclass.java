998a59d91a3319b983fcb3fc1ec6cf6b4f552e41
huseyin <hsyn_hsyn77@hotmail.com>
2022-09-18 21:44:26 +0300

    a101son

--- src/Deneme/denemeclass.java
+++ src/Deneme/denemeclass.java
@@ -2,7 +2,6 @@ package Deneme;
 
 
 import Utils.BaseStaticDriver;
-import dev.failsafe.internal.util.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -10,42 +9,45 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.junit.Assert;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
 
 public class denemeclass extends BaseStaticDriver {
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
+
 
         driver.get("https://www.a101.com.tr/");
         WebElement kabulEt = driver.findElement(By.xpath("//button[contains(text(),'Kabul Et')]"));
         kabulEt.click();
 
-        // WebElement popUp = driver.findElement(By.xpath("//div[contains(text(),'X')]"));
-        // popUp.click();
         WebElement giyimAks = driver.findElement(By.cssSelector("[title='GİYİM & AKSESUAR']"));
 
         Actions aksiyonlar = new Actions(driver);
 
         aksiyonlar.moveToElement(giyimAks).build().perform();
-        WebElement dızAltCrp = driver.findElement(By.linkText("Dizaltı Çorap"));
+        WebElement dızAltCrp = wait.until(ExpectedConditions.elementToBeClickable(By.linkText("Dizaltı Çorap")));
         aksiyonlar.moveToElement(dızAltCrp).build().perform();
         dızAltCrp.click();
 
         WebElement syhCrp = driver.findElement(By.cssSelector("[value='SİYAH']"));
         syhCrp.click();
-        WebElement syhCrp2 = driver.findElement(By.xpath("//div[@class='product-actions']"));
+        WebElement syhCrp2 = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("(//div[@class='product-actions'])[1]")));
         syhCrp2.click();
 
-        WebElement mesaj = driver.findElement(By.xpath("//div[@class='product-heading']"));
-       // Assert.("Form submited Successfully!");
+        Assert.assertTrue(driver.findElement(By.cssSelector("[class='selected-variant-text'] span")).getText().toLowerCase().contains("siyah"));
 
-        //   //div[@class='product-heading']
-        WebElement sepeteEkle = driver.findElement(By.xpath("//button[@class='add-to-basket button green block with-icon js-add-basket']"));
-        sepeteEkle.click();
-        WebElement dEt = driver.findElement(By.linkText("Sepeti Görüntüle"));
-        dEt.click();
-        WebElement sptOny = driver.findElement(By.linkText("Sepeti Onayla"));
-        sptOny.click();
-        WebElement üOlmDet = driver.findElement(By.linkText("ÜYE OLMADAN DEVAM ET"));
-        üOlmDet.click();
+
+        WebElement addToBasket = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@class='add-to-basket button green block with-icon js-add-basket']")));
+        addToBasket.click();
+        WebElement viewCart = driver.findElement(By.linkText("Sepeti Görüntüle"));
+        viewCart.click();
+        WebElement cartCheck = driver.findElement(By.linkText("Sepeti Onayla"));
+        cartCheck.click();
+        WebElement goOn = driver.findElement(By.linkText("ÜYE OLMADAN DEVAM ET"));
+        goOn.click();
         WebElement mail = driver.findElement(By.xpath("//input[@name='user_email']"));
         mail.sendKeys("huseyin77@gmail.com");
         WebElement mailBtn = driver.findElement(By.xpath("//button[@class='button block green']"));
@@ -55,33 +57,44 @@ public class denemeclass extends BaseStaticDriver {
         WebElement adressBas = driver.findElement(By.xpath("//input[@placeholder='Ev adresim, iş adresim vb.']"));
         adressBas.sendKeys("Ev");
         WebElement name = driver.findElement(By.xpath("//input[@name='first_name']"));
-        name.sendKeys("huseyin");
+        name.sendKeys("HUSEYIN");
         WebElement lastName = driver.findElement(By.xpath("//input[@name='last_name']"));
-        lastName.sendKeys("huseyin");
+        lastName.sendKeys("HUSEYIN");
         WebElement phoneNu = driver.findElement(By.xpath("//input[@name='phone_number']"));
         phoneNu.sendKeys("5552325060");
 
         WebElement cityMenu = driver.findElement(By.xpath("//select[@class='js-cities']"));
         Select city = new Select(cityMenu);
         city.selectByVisibleText("İZMİR");
+
         WebElement districtMenu = driver.findElement(By.xpath(" //select[@class='js-township']"));
         Select district = new Select(districtMenu);
         district.selectByVisibleText("KARABAĞLAR");
-        WebElement neighbourhoodMenu = driver.findElement(By.xpath(" //select[@class='js-district']"));
-        Select neighbourhoodt = new Select(neighbourhoodMenu);
-        neighbourhoodt.selectByValue("36911");
+
+
+//      --------------------------------------------------
+        WebElement neighbourhoodMenu = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//select[@name='district']")));
+        aksiyonlar.moveToElement(neighbourhoodMenu).build().perform();
+        Select neighbour = new Select(neighbourhoodMenu);
+        neighbour.selectByVisibleText("VATAN MAH");
+//      --------------------------------------------------
+
+
 
         WebElement adress = driver.findElement(By.xpath("//textarea[@name='line']"));
-        adress.sendKeys(" adres  bahar mh. 10 sk. no:3 d:5 karabağlar / İZMİR");
+        adress.sendKeys("bilinen mh. 10 sk. no:3 d:5 \nKarabağlar / IZMIR");
         WebElement postcode = driver.findElement(By.xpath("//input[@name='postcode']"));
         postcode.sendKeys(" 35000");
-        // WebElement save = driver.findElement(By.xpath("(//button[@type='button'])[6] "));
-        // save.click();
-        WebElement saveGo = driver.findElement(By.xpath("//button[@data-index='1']"));
-        saveGo.click();
 
-           WebElement save = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("(//button[@type='button'])[6] ")));
-           save.click();
+
+        WebElement save2 =wait.until(ExpectedConditions.elementToBeClickable(By.xpath("(//button[@type='button'])[6] ")));
+        save2.click();
+
+        WebElement mngBtn = driver.findElement(By.xpath("//span[contains(text(),'MNG Kargo')]"));
+        mngBtn.click();
+
+        WebElement saveGo = driver.findElement(By.xpath("//button[contains(text(),'Kaydet ve Devam Et')]"));
+        saveGo.click();
         driver.quit();
     }
 }
--- src/Utils/BaseStaticDriver.java
+++ src/Utils/BaseStaticDriver.java
@@ -1,15 +1,20 @@
 package Utils;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
 
 public class BaseStaticDriver {
 
@@ -17,20 +22,26 @@ public class BaseStaticDriver {
     public static WebDriverWait wait;
 
     static {
-
         Logger logger = Logger.getLogger("");
         logger.setLevel(Level.SEVERE);
         System.setProperty(ChromeDriverService.CHROME_DRIVER_SILENT_OUTPUT_PROPERTY, "true");
-        System.setProperty("webdriver.chrome.driver", "driver/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "driver/chromedriver");//mac (windows için "driver/chromedriver.exe" yapılacak)
         driver = new ChromeDriver();
-        driver.manage().window().maximize();//max AÇILAN WEB SAYFASINI TAM EKRAN YAPAR
+        driver.manage().window().maximize();
         driver.manage().deleteAllCookies();
         wait = new WebDriverWait(driver, Duration.ofSeconds(30));
-        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));//sadece ana sayfa yüklenirken en başta
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));// bütün sonradan görünen webElement için geçerli
+
+
+
+
+
+        driver.manage();
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
     }
 
 
-}
+
+    }
 
 
