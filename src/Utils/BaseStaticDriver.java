57008d4cbf7518b63be65d3b289ab1d54a577ec9
huseyin <hsyn_hsyn77@hotmail.com>
2022-09-19 12:58:19 +0300

    a101 73.SIRAYA BAKILACAK

--- src/Utils/BaseStaticDriver.java
+++ src/Utils/BaseStaticDriver.java
@@ -31,11 +31,6 @@ public class BaseStaticDriver {
         driver.manage().deleteAllCookies();
         wait = new WebDriverWait(driver, Duration.ofSeconds(30));
 
-
-
-
-
-        driver.manage();
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
     }
